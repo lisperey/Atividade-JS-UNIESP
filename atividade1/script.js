d40fd2850674dc197c93aff7414ef3cc9f209bb8
lisperey <pilrss16@gmail.com>
2022-08-29 15:51:36 -0300

    first commit

---
+++ atividade1/script.js
@@ -0,0 +1,71 @@
+var alturaH = [];
+var alturaM = [];
+var alturaT = [];
+
+for(let i = 0; i <15; i++){
+    var a = prompt("Informe sua altura: ");
+    var s = prompt("Informe seu sexo(m- masculino e f- feminino): ");
+
+    alturaT.push(a);
+
+    if(s.toLowerCase() == 'm'){
+
+        alturaH.push(a);
+    }
+    else if(s.toLowerCase() == 'f'){
+        alturaM.push(a);
+    }
+    else{
+        while(s.toLowerCase() != 'f' && s.toLowerCase() != 'm'){
+            alert("Sexo não é valido");
+            s = prompt("Informe seu sexo(m- masculino e f- feminino): "); 
+        }
+        if(s.toLowerCase() == 'm'){
+
+            alturaH.push(a);
+        }
+        else if(s.toLowerCase() == 'f'){
+            alturaM.push(a);
+        }
+
+        
+    }
+}
+
+let maxA = alturaT.reduce(function (prev, current) { 
+	return prev > current ? prev : current;
+
+});
+
+
+console.log(`Maior altura do grupo: ${maxA}.`);
+
+
+var somahN = 0;
+for(x in alturaH){
+    
+    somahN += parseFloat(alturaH[x]); 
+}
+
+let media = somahN/alturaH.length;
+if(alturaH.length == 0){
+    console.log("Não tem homens na lista.")
+
+}else{
+    console.log(`Média da altura dos homens: ${media}.`);
+
+}
+
+if(alturaM.length == 0){
+    console.log("Não tem mulheres na lista.")
+
+}
+else if(alturaM.length == 1){
+    console.log(`Na lista tem ${alturaM.length} mulher.`);
+    
+
+}
+else{
+    console.log(`Na lista tem ${alturaM.length} mulheres.`);
+
+}
